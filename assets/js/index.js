16fc22c03e4bf75eb51cf87c91388a5a57123296
杨阳 <mapleqlue@163.com>
2016-08-04 00:31:12 +0800

    new code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,16 @@
 ;(function(exports){
+	var header = exports.document.getElementsByClassName('header')[0];
 	var nav = exports.document.getElementsByClassName('nav')[0];
 	var lis = nav.getElementsByTagName('li');
 	var body = exports.document.getElementsByClassName('body')[0];
 	var conts = body.getElementsByTagName('li');
 	for (var i = 0; i < lis.length; i++) {
-		lis[i].onmouseenter = function(cur){
+		lis[i].onclick = function(cur){
 			return function(){
 				for (var i = 0; i < conts.length; i++) {
 					hideCont(i);
 				}
 				showCont(cur);
-			};
-		}(i);
-		lis[i].onmouseout = function(cur){
-			return function(){
-				setTimeout(function(){
-					hideCont(cur);
-				}, 10000);
 			};
 		}(i);
 	}
@@ -27,11 +21,15 @@
 	};
 
 	var showCont = function(cur){
+		header.className += ' active';
+		body.className += ' active';
 		if (conts[cur]) {
 			conts[cur].className = 'show';
 		}
 	};
 	var hideCont = function(cur){
+		header.className = header.className.replace(' active', '');
+		body.className = body.className.replace(' active', '');
 		if (conts[cur]) {
 			conts[cur].className = '';
 		}
