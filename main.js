f4efae26c82f158865ab117ecf4d96501e821ef6
mapleque <mapleque@163.com>
2017-08-17 17:24:35 +0000

    link

--- main.js
+++ main.js
@@ -5,7 +5,9 @@
     image: {}
   }
   var links = {
-    image: '//blog.mapleque.com'
+    image: '//blog.mapleque.com',
+    git: '//github.com/mapleque',
+    blog: '//blog.mapleque.com',
   }
 
   var canvas = document.getElementById('canvas')
@@ -42,6 +44,8 @@
     }
   }
 
+  /*
+  // linear background
   var drawBackground = function () {
     var w = canvas.width
     var h = canvas.height
@@ -74,10 +78,166 @@
     ]
     fillbk(linears)
   }
+  */
+  // binary background
+  var drawBackground = function () {
+    var w = canvas.width
+    var h = canvas.height
+    context.fillStyle = '#000'
+    context.fillRect(0,0,w,h/2)
+  }
+
+  var drawLogo = function () {
+    var w = canvas.width
+    var h = canvas.height
+    var gradient = context.createLinearGradient(0, 0, 0, h)
+    gradient.addColorStop(0, '#fff')
+    gradient.addColorStop(0.5, '#fff')
+    gradient.addColorStop(0.5, '#000')
+    gradient.addColorStop(1, '#000')
+    context.fillStyle = gradient
+    context.font = 'bold 100px Arial'
+    context.fillText('Mapleque', w/2 - 232, h/2+30)
+  }
+
+  var drawBlog = function () {
+    var w = canvas.width
+    var h = canvas.height
+    var px = w/2-150
+    var py = h*3/4 -50
+    scales.blog= {
+      x: px,
+      y: py,
+      w: 84,
+      h: 60
+    }
+
+    // outline
+    context.beginPath()
+    context.rect(px,py,84,60)
+    context.closePath()
+    context.lineWidth = 1
+    context.strokeStyle = '#000'
+    context.stroke()
+
+    // 2 black key
+    context.fillStyle = '#000'
+    context.fillRect(px+12,py,12,50)
+    context.fillRect(px+36,py,12,50)
+    context.fillRect(px+60,py,12,50)
+
+    // 3 white key
+    context.moveTo(px+18,py+60)
+    context.lineTo(px+18,py+45)
+    context.moveTo(px+44,py+60)
+    context.lineTo(px+44,py+45)
+    context.moveTo(px+66,py+60)
+    context.lineTo(px+66,py+45)
+    context.stroke()
+    /*
+    context.fillStyle = '#000'
+    context.font = 'bold 40px Arial'
+    context.fillText('Blog', px, py)
+    */
+  }
+  var drawGit = function () {
+    var w = canvas.width
+    var h = canvas.height
+    var px = w/2+80
+    var py = h*3/4-50
+    scales.git= {
+      x: px,
+      y: py,
+      w: 84,
+      h: 60
+    }
+    // outline
+    context.beginPath()
+    context.rect(px,py,84,60)
+    context.closePath()
+    context.lineWidth = 1
+    context.strokeStyle = '#000'
+    context.stroke()
+
+    // inline
+    context.moveTo(px,py+12)
+    context.lineTo(px+84,py+12)
+    context.moveTo(px,py+24)
+    context.lineTo(px+84,py+24)
+    context.moveTo(px,py+36)
+    context.lineTo(px+84,py+36)
+    context.moveTo(px,py+48)
+    context.lineTo(px+84,py+48)
+    context.moveTo(px,py+60)
+    context.lineTo(px+84,py+60)
+    context.moveTo(px+12,py)
+    context.lineTo(px+12,py+60)
+    context.moveTo(px+24,py)
+    context.lineTo(px+24,py+60)
+    context.moveTo(px+36,py)
+    context.lineTo(px+36,py+60)
+    context.moveTo(px+48,py)
+    context.lineTo(px+48,py+60)
+    context.moveTo(px+60,py)
+    context.lineTo(px+60,py+60)
+    context.moveTo(px+72,py)
+    context.lineTo(px+72,py+60)
+    context.stroke()
+
+    // pos stone
+    var xm = { M:0, N:1,O:2,P:3,Q:4,R:5,S:6,T:7 }
+    var ym = { 19:0,18:1,17:2,16:3,15:4,14:5 }
+    var p = function (bw, p) {
+      var x = xm[p[0]]
+      var y = ym[p[1]+p[2]]
+      bw ? black(x,y) : white(x,y)
+    }
+    var black = function(x,y) {
+      context.beginPath()
+      context.fillStyle = '#000'
+      context.arc(px+x*12,py+y*12, 6, 0, 2*Math.PI, false)
+      context.closePath()
+      context.fill()
+    }
+    var white = function(x,y) {
+      context.beginPath()
+      context.fillStyle = '#fff'
+      context.lineWidth = 1
+      context.strokeStyle = '#000'
+      context.arc(px+x*12,py+y*12, 6, 0, 2*Math.PI, false)
+      context.closePath()
+      context.stroke()
+      context.fill()
+    }
+
+    // play
+    var b = true, w = false
+    p(b,'R16')
+    p(w,'P16')
+    p(b,'P17')
+    p(w,'Q16')
+    p(b,'Q17')
+    p(w,'O16')
+    p(b,'O17')
+    p(w,'R15')
+    p(b,'N16')
+    p(w,'R17')
+    p(b,'S16')
+    p(w,'R18')
+
+    /*
+    context.fillStyle = '#000'
+    context.font = 'bold 40px Arial'
+    context.fillText('Git', px, py)
+    */
+  }
 
   var draw = function () {
     drawBackground()
-    drawImage()
+    // drawImage()
+    drawLogo()
+    drawBlog()
+    drawGit()
   }
 
   // event listener
@@ -97,9 +257,15 @@
       document.body.style.cursor = ''
       currentLink = ''
     }
-    if (inscale(pos, scales.image)) {
-      pointer(links.image)
-    } else {
+    var isP = false
+    for (var key in scales) {
+      if (scales.hasOwnProperty(key) && inscale(pos, scales[key])) {
+        pointer(links[key])
+        isP = true
+        break
+      }
+    }
+    if (!isP) {
       normal()
     }
   }
